b33e3857a24851555893b5933d9d09febc0791d1
rlilla <ricardo_lilla@hotmail.com>
2021-04-08 22:32:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,5 @@ import sys
 import datalayerprovider.register_provider
 
 if __name__ == '__main__':
-    f=open('usr/teste.txt','w')
-    f.write('teste123')
-    f.close()
     datalayerprovider.register_provider.run()
     
